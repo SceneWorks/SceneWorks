c5e59a27014c7f176c812e06d0edb91b4bd03cd3
Michael Trefry <michael@trefry.net>
2026-07-30 18:00:18 -0400

    chore(deps): pin rung-4 timing evidence (sc-16154)
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "1ca66939c7ea21f91f44d1ec2bbfef70507dd0bc";
+pub const INFERENCE_PIN: &str = "31c663dd6ce4eb55d5bd331bf716bb6de53d5aa2";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
 pub const COMPARISON_OUTPUT_BIAS_PARAMETER: &str = "comparisonOutputBias";
