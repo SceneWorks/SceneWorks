1de8d24c939e258ed706c0c04a0f27c537671a43
Michael Trefry <michael@trefry.net>
2026-08-10 19:39:19 -0400

    chore(inference): pin SCAIL runner Python fix
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "02219a173c570e0fbf7fdc5dae77b187073a732f";
+pub const INFERENCE_PIN: &str = "7c50e773115d34cd3869e1c6dc7bbf4bb879b0a0";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
