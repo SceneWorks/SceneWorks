3d00244260aeb82747b9c61b1df0fc2d377a0be2
Michael Trefry <michael@trefry.net>
2026-08-11 06:51:49 -0400

    chore(sc-18306): pin verified inference candidate
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "52f94dc7c105c8afe732ba5901fdbb7563fed640";
+pub const INFERENCE_PIN: &str = "8c2c37590ea5681dd918a05997cd66b8cd2eecda";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
