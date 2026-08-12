0f208cfd5efbb0a216f1012cd3dbb9cffbad2af3
Michael Trefry <michael@trefry.net>
2026-08-11 20:23:58 -0400

    chore(sc-17087): pin Z-Image behavior merge
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "01085dde24cb3405b53f680b2a7c1f54b2f2e7aa";
+pub const INFERENCE_PIN: &str = "56285e040a57f71c628f3ee15e983890a509cc27";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
