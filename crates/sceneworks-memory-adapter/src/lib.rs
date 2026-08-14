3c50a2360316e33f3a0a531b44205341e75011ef
Michael Trefry <michael@trefry.net>
2026-08-14 08:08:22 -0400

    chore(inference): repin permanent parity runtime
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "82b45a5f621608c037b2b2286b3c8b078d9bdf81";
+pub const INFERENCE_PIN: &str = "0e4adc6f05884e6891e29dfecd32ee695efe8b18";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
