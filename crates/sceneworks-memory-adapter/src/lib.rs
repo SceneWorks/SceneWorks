d6bf519420b6b03b7fa3f9b53742a0acb37e96a8
Michael Trefry <michael@trefry.net>
2026-08-15 07:37:29 -0400

    chore: pin active memory stack to inference freeze
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "7c45f22aa671e16de72929e1781ac66994c21f7f";
+pub const INFERENCE_PIN: &str = "8e6497f2f8f915ffe24b104c1b4f1b4dfe9a6b8d";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
