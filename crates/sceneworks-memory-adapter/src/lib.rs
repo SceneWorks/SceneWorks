ae7ab8186d6b5ca3fdec9c99cd6cecdab15dcce6
Michael Trefry <michael@trefry.net>
2026-08-20 14:53:12 -0400

    chore(sc-16948): pin final inference main
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "56e4c32d8b966536f60dcd0a7920c166938faef3";
+pub const INFERENCE_PIN: &str = "c3bfb064843f517abfe65660be57456214982770";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
