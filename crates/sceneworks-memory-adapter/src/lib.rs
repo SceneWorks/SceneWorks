eeb1fe0d0dd188199d43825bcb6cbcb56fe0a030
Michael Trefry <michael@trefry.net>
2026-08-20 11:06:49 -0400

    chore(inference): consume SC-17181 preview correction
    Signed-off-by: Michael Trefry <michael@trefry.net>

--- crates/sceneworks-memory-adapter/src/lib.rs
+++ crates/sceneworks-memory-adapter/src/lib.rs
@@ -9,7 +9,7 @@ use std::io::{self, Read};
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-pub const INFERENCE_PIN: &str = "6f47e6707589158b89d0f238c5ef56be6f1bdd7a";
+pub const INFERENCE_PIN: &str = "a829226bdd81a9a46571bc0abfa2c423f35901fa";
 pub const QWEN_REPOSITORY: &str = "SceneWorks/qwen-image-mlx";
 pub const FLUX2_REPOSITORY: &str = "SceneWorks/flux2-dev-mlx";
 pub const KREA_REPOSITORY: &str = "SceneWorks/krea-2-turbo-mlx";
